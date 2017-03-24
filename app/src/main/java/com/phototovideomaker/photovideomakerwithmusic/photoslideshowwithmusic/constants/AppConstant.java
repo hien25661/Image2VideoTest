7b12c63b883a8a72aeef69b3bf45a0b736d7f3b5
hien.nv <hien.nv@vn.gnt-global.com>
2017-03-24 14:35:51 +0700

    Delete

--- app/src/main/java/com/phototovideomaker/photovideomakerwithmusic/photoslideshowwithmusic/constants/AppConstant.java
+++
@@ -1,9 +0,0 @@
-package com.phototovideomaker.photovideomakerwithmusic.photoslideshowwithmusic.constants;
-
-/**
- * Created by SF on 05/05/2016.
- */
-public class AppConstant {
-    public static final int IMAGE_MAX_WIDTH = 720;
-    public static final int IMAGE_MAX_HEIGHT = 1280;
-}
--- app/src/main/java/com/phototovideomaker/photovideomakerwithmusic/photoslideshowwithmusic/models/User.java
+++
@@ -1,8 +0,0 @@
-package com.phototovideomaker.photovideomakerwithmusic.photoslideshowwithmusic.models;
-
-/**
- * Created by SF on 05/05/2016.
- */
-public class User {
-    String username;
-}
